4585c90be3062a09ca25ede1edf6b544edc40fb3
Murat Çeliktepe <celiktepemurat@gmail.com>
2021-09-13 22:11:21 +0300

    turn into typescript

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract AirdropContract {
+    // define merkle root variable
+    // setMerkleRoot function - only admin can call
+    // constructor - merkle root will be set here, also owner
+    // GETTER - get merkle root
+    // event call - when merkle root is changed 
+
+
+}
