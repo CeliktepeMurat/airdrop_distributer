75607378570f671df1b13b1a537db97624c57777
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-09-17 13:43:20 +0300

    merkle tree script

--- scripts/getMerkleProof.ts
+++ scripts/getMerkleProof.ts
@@ -1,15 +1,16 @@
-import { MerkleTree } from 'merkletreejs'
-import SHA256 from 'crypto-js/sha256'
+import { MerkleTree } from 'merkletreejs';
+import SHA256 from 'crypto-js/sha256';
 
-const leaves = ['a', 'b', 'c'].map(x => SHA256(x))
-const tree = new MerkleTree(leaves, SHA256)
-const root = tree.getRoot().toString('hex')
-const leaf = SHA256('a')
-const proof = tree.getProof(leaf)
-console.log(tree.verify(proof, leaf, root)) // true
+import * as accounts from '../utils/accounts.json';
 
+const leaves = accounts.map((account) => SHA256(account));
+const tree = new MerkleTree(leaves, SHA256);
+const root = tree.getRoot().toString('hex');
+const leaf: any = SHA256('LEAF');
+const proof = tree.getProof(leaf);
+console.log(tree.verify(proof, leaf, root)); // true
 
-const badLeaves = ['a', 'x', 'c'].map(x => SHA256(x))
-const badTree = new MerkleTree(badLeaves, SHA256)
-const badLeaf = SHA256('x')
-const badProof = tree.getProof(badLeaf)
+const badLeaves = accounts.map((account) => SHA256(account));
+const badTree = new MerkleTree(badLeaves, SHA256);
+const badLeaf: any = SHA256('x');
+const badProof = tree.getProof(badLeaf);
